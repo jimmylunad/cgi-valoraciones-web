dac8479599c60bfe14d1dce612c970dd37279c05
Freddy Contreras <fcontreras@solera.pe>
2021-06-24 19:35:16 -0500

    - Validación de campos

--- src/modules/Auth/Login/index.tsx
+++ src/modules/Auth/Login/index.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { Container, Grid } from "@material-ui/core";
 import Button from "components/Button";
 import { FormInput, FormCheckbox } from "components/Form";
@@ -5,29 +6,45 @@ import { Link } from "react-router-dom";
 import "./styles.scss";
 
 const Login = ():JSX.Element => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <Container maxWidth="md" className="container">
-      <Grid container direction="column" alignItems="center">
-        <img className="logo" src="https://via.placeholder.com/150" alt="logo"></img>
-        <h6 className="title">Iniciar sesión</h6>
-        <span className="text">Ingresa tus credenciales</span>
-        <FormInput 
-          type="text"
-          className="form-input" 
-          placeholder="Ingresa correo electrónico" />
-        <FormInput 
-          type="password"
-          className="form-input" 
-          placeholder="Ingresa contraseña" />
-        <Grid item container justify="space-between">
-          <FormCheckbox>Recuérdame</FormCheckbox>
-          <Link className="link" to="/auth/password">Olvidé mi contraseña</Link>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container maxWidth="md" className="container">
+        <Grid container direction="column" alignItems="center">
+          <img className="logo" src="https://via.placeholder.com/150" alt="logo"></img>
+          <h6 className="title">Iniciar sesión</h6>
+          <span className="text">Ingresa tus credenciales</span>
+            <FormInput 
+              type="text"
+              className={"form-input " + (errors.email ? "--error" : '') } 
+              placeholder="Ingresa correo electrónico" 
+              {...register("email", { 
+                required: true, 
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Formato de correo incorrecto"
+                }})
+              }
+              />
+            <FormInput 
+              type="password"
+              className={"form-input " + (errors.password ? "--error" : '') } 
+              placeholder="Ingresa contraseña"
+              {...register("password", { required: true })}
+              />
+            <Grid item container justify="space-between">
+              <FormCheckbox>Recuérdame</FormCheckbox>
+              <Link className="link" to="/auth/password">Olvidé mi contraseña</Link>
+            </Grid>
+            <br></br>
+            <br></br>
+            <Button className="btn --primary" type="submit"> Ingresar</Button>
         </Grid>
-        <br></br>
-        <br></br>
-        <Button className="btn --primary"> Ingresar</Button>
-      </Grid>
-    </Container>
+      </Container>
+    </form>
   )
 };
 
--- src/modules/Auth/ResetPassword/index.tsx
+++ src/modules/Auth/ResetPassword/index.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { Container, Grid } from "@material-ui/core";
 import Button from "components/Button";
 import { FormInput } from "components/Form";
@@ -5,38 +6,47 @@ import { useHistory } from "react-router";
 import "./styles.scss";
 
 const ResetPassword = ():JSX.Element => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const history = useHistory();
 
-  const goBack = () => {
-    ;
-  }
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <Container maxWidth="md" className="container">
-      <Grid container direction="column" alignItems="center">
-        <img className="logo" src="https://via.placeholder.com/150" alt="logo"></img>
-        <h6 className="title">¿Olvidaste tu contraseña?</h6>
-        <span className="text">Ingresa tu correo electrónico registrado</span>
-        <FormInput 
-          type="text"
-          className="form-input" 
-          placeholder="Ingresa correo electrónico" />
-        
-        <br></br>
-        <Grid container>
-          <Grid item xs={8}>
-            <Button className="btn --primary w-95"> Recuperar contraseña</Button>
-          </Grid>
-          <Grid item xs={4}>
-            <Button className="btn --cancel" 
-              onClick={() => {
-              history.push('/auth/login')
-              }}> 
-              Cancelar
-          </Button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container maxWidth="md" className="container">
+        <Grid container direction="column" alignItems="center">
+          <img className="logo" src="https://via.placeholder.com/150" alt="logo"></img>
+          <h6 className="title">¿Olvidaste tu contraseña?</h6>
+          <span className="text">Ingresa tu correo electrónico registrado</span>
+          <FormInput 
+            type="text"
+            className={"form-input " + (errors.email ? "--error" : '') } 
+            placeholder="Ingresa correo electrónico" 
+            {...register("email", { 
+              required: true, 
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Formato de correo incorrecto"
+              }})
+            }
+          />
+          <br></br>
+          <Grid container>
+            <Grid item xs={8}>
+              <Button className="btn --primary w-95" type="submit"> Recuperar contraseña</Button>
+            </Grid>
+            <Grid item xs={4}>
+              <Button className="btn --cancel" 
+                onClick={() => {
+                history.push('/auth/login')
+                }}> 
+                Cancelar
+            </Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </form>
   )
 };
 
