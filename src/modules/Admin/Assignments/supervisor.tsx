09cf7f748bf4575583204053e3b06556b5e79ebe
Freddy Contreras <fcontreras@solera.pe>
2021-07-04 21:15:37 -0500

    - Actualización del listado home

--- src/modules/Admin/Assignments/supervisor.tsx
+++ src/modules/Admin/Assignments/supervisor.tsx
@@ -1,18 +1,39 @@
 import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container, Grid } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import Button from "components/Button";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import useFetch from "services/useFetch";
 import Header from "shared/Header";
 import Tabs, { TabOption } from "shared/Tabs";
 import { Assignment } from "types/assignment";
 
+const Loading = (): JSX.Element => (
+  <li className="card">
+    <Grid container>
+      <Grid item xs={2}>
+        <Skeleton className="skeleton-image" height={50} width={30}></Skeleton>
+      </Grid>
+      <Grid item>
+        <Skeleton height={25} width={250}></Skeleton>
+        <Skeleton height={25} width={250}></Skeleton>
+        <Skeleton height={25} width={230}></Skeleton>
+      </Grid>
+    </Grid>
+  </li>
+)
+
 const SupervisorAssignments = (): JSX.Element => {
   
   const history = useHistory();
-  const [currentFilter, setCurrentFilter] = useState<string>('doned');
+  const [currentFilter, setCurrentFilter] = useState<string>("doned");
+  const [dashboardData, setDashboardData] = useState<{
+    canceled: number;
+    doned: number;
+    scheduled: number;
+  }>();
   const OPTIONS_TABS: TabOption[] = [
     {title: 'HOY', link: '/programaciones'},
     {title: 'Semana', link: '/semana'},
@@ -25,22 +46,42 @@ const SupervisorAssignments = (): JSX.Element => {
     config: {
       url: '/v1/app/supervisor/dashboard',
     }
-  })
+  });
 
-  const getData = useCallback(async () => {
+  const { fetch: fetchData, loading:loadingData } = useFetch({
+    loading: true,
+    config: {
+      url: '/v1/app/assignment',
+    }
+  });
+
+  const getDashboard = useCallback(async () => {
     const response = await fetch({
       params: {
-        type: currentFilter
+        type: 1,
       }
     })
 
-    // setAssignments(response.data);
-    console.log("response", response)
-  }, []);
+    setDashboardData(response.data);
+  }, [currentFilter, fetch]);
+
+  const getData = useCallback(async () => {
+    const response = await fetchData({
+      params: {
+        type: 1,
+        show: currentFilter
+      }
+    })
+    setAssignments(response.data);
+  }, [currentFilter])
 
   useEffect(() => {
     getData();
-  }, [currentFilter, getData])
+  }, [currentFilter])
+
+  useEffect(() => {
+    getDashboard();
+  }, [])
 
   return (
   <>
@@ -49,24 +90,34 @@ const SupervisorAssignments = (): JSX.Element => {
       <Tabs options={OPTIONS_TABS} />
       <Container maxWidth="md" className="tab__container">
         <div className="tab__list">
-          {/* {
-            assignments.length === 0 && 
-            <p className="tab__empty">No tiene asignaciones pendientes</p>
-          } */}
+          
           <Grid container className="filters_buttons">
             <Grid xs={6}>
-              <Button className="btn --blue w-98">
-                <span>51</span> Realizadas
+              <Button 
+                className="btn --blue w-98"
+                onClick={() => {setCurrentFilter('doned')}}
+              >
+                <span>{dashboardData?.doned}</span> Realizadas
               </Button>
             </Grid>
             <Grid container xs={6} justify="flex-end">
-              <Button className="btn --yellow w-98"><span>180</span> Programadas</Button>
-              <Button className="btn --red w-98"><span>35</span> No realizadas</Button>
+              <Button 
+                className="btn --yellow w-98"
+                onClick={() => {setCurrentFilter('scheduled')}}
+                ><span>{dashboardData?.scheduled}</span> Programadas</Button>
+              <Button 
+                className="btn --red w-98"
+                onClick={() => {setCurrentFilter('canceled')}}
+              ><span>{dashboardData?.canceled}</span> No realizadas</Button>
             </Grid>
           </Grid>
+          {
+            !loadingData && assignments.length === 0 && 
+            <p className="tab__empty">No tiene asignaciones pendientes</p>
+          }
           <ul> 
             {
-             assignments.map((assignment, index) => (
+              !loadingData ? assignments.map((assignment, index) => (
                 <li key={index*2} className="card" onClick={() => { history.push('/programaciones/informacion/' + assignment.id )}}>
                   <div className="assignment">
                     <div className="assignment__ico">
@@ -85,6 +136,10 @@ const SupervisorAssignments = (): JSX.Element => {
                     </div>
                   </div>
                 </li>
+              ))
+              :
+              Array(3).fill(2).map((e, index) => (
+                <Loading key={index*2}/>
               ))
             }
           </ul>
